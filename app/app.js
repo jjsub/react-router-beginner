658b00ae71ea6f66fac77c3111e752c8c1cde928
jjsub <jsamesub@gmail.com>
2016-07-22 11:42:06 -0500

    Add route and new components

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import { Router,
-         Route,
-         Link,
-         IndexRoute,
-         hashHistory,
-         browserHistory } from 'react-router'
+import {
+  Router,
+  Route,
+  Link,
+  IndexRoute,
+  hashHistory,
+  browserHistory } from 'react-router'
 
-const App = () => <h1>Hello World!</h1>
-
+class App extends Component {
+ render() {
+   return (
+     <Router history={hashHistory}>
+       <Route path='/' component={Home} />
+       <Route path='/address' component={Address} />
+     </Router>
+   )
+ }
+}
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
 export default App
