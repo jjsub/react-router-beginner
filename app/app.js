b0710de7c2fad27c93ec2c6e93624b089f08e8ea
jjsub <jsamesub@gmail.com>
2016-07-22 11:45:44 -0500

    Add 404 route

--- app/app.js
+++ app/app.js
@@ -13,10 +13,13 @@ class App extends Component {
      <Router history={hashHistory}>
        <Route path='/' component={Home} />
        <Route path='/address' component={Address} />
+       <Route path='*' component={NotFound} />
      </Router>
    )
  }
 }
-const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are located at 555 Jackson St.</h1>
+const Home = () => <h1>Hello from Home!</h1>;
+const Address = () => <h1>We are located at 555 Jackson St.</h1>;
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>);
 export default App
